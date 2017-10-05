b04568158b2ca8370864b8c733e315db9fcaac35
cuser <amal0109@gmail.com>
2017-10-05 13:07:07 +0530

    fsl problem resolved
    avoided frequent visit to dom.

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randompizza=document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < randompizza.length; i++) {
+      var dx = randompizza[i].offsetWidth + dx) + 'px';
+      randompizza[i].style.width = newwidth;
     }
   }
 
